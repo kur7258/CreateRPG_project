bf42a29053371c93be680f7d20797c298ea49321
Kim-Jae-A <kimjea1004@naver.com>
2020-11-10 21:47:52 +0900

    Make_Array

---
+++ tut/KJA/KimJA_2048/KimJA_2048/InPutKey.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KimJA_2048
+{
+    class InPutKey
+    {
+    }
+}
---
+++ tut/KJA/KimJA_2048/KimJA_2048/PrintArray.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace KimJA_2048
+{
+    class PrintArray
+    {        
+        static void Main(string[] args)
+        {
+            int[][] array = new int[4][];
+            array[0] = new int[] { 0, 0, 0, 0 };
+            array[1] = new int[] { 0, 0, 0, 0 };
+            array[2] = new int[] { 0, 0, 0, 0 };
+            array[3] = new int[] { 0, 0, 0, 0 };
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length; j++)
+                {
+                    Console.Write(" {0} ", array[i][j]);
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
