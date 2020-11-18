422705e8eab4638c8ba5afa126ce0b66b035a813
ohheaynjin <dhguswls5353@naver.com>
2020-11-19 01:18:01 +0900

    ohj

---
+++ tut/OHJ/2048/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Class1
+{
+
+	public Class1()
+	{
+        Console.WriteLine("fdfssfdfsdsf");
+	}
+    public void PrintTest()
+    {
+        Console.WriteLine("fdfssfdfsdsf");
+    }
+}
